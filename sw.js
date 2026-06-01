97ff80c93f46ad9e4c0eafdafcf1baf111eb7a22
Tunlinaung192 <tun123287@gmail.com>
2026-06-01 15:43:40 +0630

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+// sw.js - Service Worker for Offline PWA Support
+const CACHE_NAME = 'tx-book-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './config.js',
+  './auth.js',
+  './transactions.js',
+  './ui.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
